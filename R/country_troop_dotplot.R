9a00e195527212aaa06d59136bb569d1e9bb4250
etwerner <etwerner@ucsd.edu>
2018-08-16 16:10:39 -0700

    line and dot plot functions

--- R/country_troop_dotplot.R
+++ R/country_troop_dotplot.R
@@ -1,26 +1,7 @@
-country_troop_lineplot <- function(iiss_afghan){
+country_troop_dotplot <- function(iiss_afghan){
 
   troops_subset <- subset(iiss_afghan, iiss_afghan$troops_total > 0)
   troops_subset <- troops_subset %>% filter(troops_subset$year > 2000 & troops_subset$year < 2006)
-
-  percentages <- list()
-  index <- 1
-  countries <- sort(unique(troops_subset$country))
-  for(state in countries){
-    temp <- troops_subset[which(troops_subset$country==state),]
-    p <- temp$troops_afghan_ratio*100
-    avg <- mean(p)
-    percentages[[index]] <- data.frame(country = state, percent = avg)
-    index <- index + 1
-  }
-  my_df <- percentages[[1]]
-  for(i in 2:length(percentages)){
-    my_df <- rbind(my_df,percentages[[i]])
-  }
-  temp <- my_df[order(-my_df$percent),]
-  top <- head(temp,10)
-  top <- data.frame(top)
-  top_ten <- top$country
 
   percentages <- list()
   index <- 1
@@ -32,6 +13,10 @@ country_troop_lineplot <- function(iiss_afghan){
                  "Latvia","Lithuania","Luxembourg","Switzerland","Estonia","Hungary",
                  "Ireland","Korea, Republic of","Kuwait",
                  "Macedonia, Former Yugoslav Republic of","Slovakia","Slovenia")
+
+
+
+
 
   years <- c(2001,2002,2003,2004,2005)
   for(yr in years){
@@ -49,23 +34,33 @@ country_troop_lineplot <- function(iiss_afghan){
     my_df <- rbind(my_df,percentages[[i]])
   }
 
-  top <- my_df[which(my_df$country %in% top_ten),]
+  line <- list()
+  index <- 1
+  for(yr in years){
+    filtered <- troops_subset[which(troops_subset$year==yr),]
+    value <- mean(filtered$troops_afghan_total)
+    line[[index]] <- data.frame(year = yr, avg = value)
+    index <- index + 1
+  }
+  line_df <- line[[1]]
+  for(i in 2:length(line)){
+    line_df <- rbind(line_df,line[[i]])
+  }
+
+  #temp <- my_df[which(my_df$total > 0),]
+  Country <- my_df$country
 
   my_plot <- ggplot() +
-    geom_line(aes(x=year,y=total,group=country), data = my_df, colour = alpha("grey", 0.7)) +
-    geom_line(aes(x=year,y=total,colour=country), data = top) +
-    geom_point() +
+    geom_point(data = my_df, aes(x=my_df$year,y=my_df$total,col=Country)) +
+    geom_smooth(data = line_df, aes(x=line_df$year,y=line_df$avg)) +
     labs(title="Troop Contributions",subtitle="2001-2005",
          y="Total Troops",x = "Year") +
     theme_linedraw() +
     theme(axis.text.x = element_text(size = 12,vjust=0.6),
           axis.text.y = element_text(size = 12),
-          legend.position="bottom",
-          legend.text=element_text(size=7),
-          legend.title = element_blank())
+          legend.position="none",)
   my_plot
 
   return(my_plot)
 
 }
-
