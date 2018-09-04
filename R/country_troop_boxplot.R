ab114a8c7ace14b51b73fdcce92e99820f36fb41
etwerner <etwerner@ucsd.edu>
2018-09-03 21:57:01 -0700

    erin updates

--- R/country_troop_boxplot.R
+++ R/country_troop_boxplot.R
@@ -1,12 +1,13 @@
 country_troop_boxplot <- function(iiss_afghan){
 
   troops_subset <- subset(iiss_afghan, iiss_afghan$troops_total > 0)
-  troops_subset <- troops_subset %>% filter(troops_subset$year > 2000 & troops_subset$year < 2006)
+  #troops_subset <- troops_subset %>% filter(troops_subset$year > 2000 & troops_subset$year < 2006)
 
   percentages <- list()
   index <- 1
   countries <- sort(unique(troops_subset$country))
-  years <- c(2001,2002,2003,2004,2005)
+  #years <- c(2001,2002,2003,2004,2005)
+  years <- sort(unique(troops_subset$year))
   for(yr in years){
     filtered <- troops_subset[which(troops_subset$year==yr),]
     for(state in countries){
@@ -40,7 +41,7 @@ country_troop_boxplot <- function(iiss_afghan){
     ylim(0,max(my_df$log_tot)+0.5) +
     geom_text_repel(aes(label = my_df$outlier), point.padding = 2) +
     labs(title="Average country contributions to ISAF by year",
-         subtitle="2001-2005",
+         subtitle="2001-2014",
          y="Log National Troop Deployment",x = "Year")
   g
 
